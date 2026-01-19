6c0c3264a97b89865d64db67abb523b97514983e
thaovo296 <thaovo2962002@gmail.com>
2026-01-19 14:11:28 +0200

    calculator add substract

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+print("Simple Calculator")
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Add:", add(a, b))
+print("Subtract:", sub(a, b))
